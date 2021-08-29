b89edee01c8b3529c5ce2eff1d49a9e17af52d0b
aklycbt <87492917+aklycbt@users.noreply.github.com>
2021-08-29 21:06:36 +0800

    Add files via upload

--- ts3/src/index.js
+++ ts3/src/index.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './components/Login';
+import ListAll from './components/ListAll';
+import LandingAdmin from './components/LandingAdmin';
+import LandingGeneral from './components/LandingGeneral';
 
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+
+<Router>
+    <div> 
+        <Route exact path='/' component={Login} />
+        <Route exact path='/LandingAdmin/:user' component={LandingAdmin} />
+        <Route exact path='/LandingGeneral/:user' component={LandingGeneral} />
+      
+    </div>
+</Router>,
+document.getElementById('root'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
