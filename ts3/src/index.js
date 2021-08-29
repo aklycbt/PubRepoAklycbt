e06be526ff6bb52266f8e26bb71d3a6a2b2ef0fa
aklycbt <87492917+aklycbt@users.noreply.github.com>
2021-08-29 21:10:36 +0800

    Add files via upload

--- ts3/src/index.js
+++ ts3/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login';
-import ListAll from './components/ListAll';
 import LandingAdmin from './components/LandingAdmin';
 import LandingGeneral from './components/LandingGeneral';
 
