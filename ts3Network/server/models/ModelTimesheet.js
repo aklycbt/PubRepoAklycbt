d8672dce55cdfb241564d2c1e085803448516ef9
aklycbt <87492917+aklycbt@users.noreply.github.com>
2021-08-29 20:54:28 +0800

    Add files via upload

--- ts3Network/server/models/ModelTimesheet.js
+++ ts3Network/server/models/ModelTimesheet.js
@@ -1,1 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+var ModelTimesheet = new Schema({ 
+
+        key: '',
+        staffCode: '',
+        group: '',
+        project: '',
+        activity: '',
+        txDate: {type: Date},
+        timeSpent: '',
+        submitted: false
+},
+{
+    collection: 'timesheet'
+});
+
+module.exports = mongoose.model('ModelTimesheet', ModelTimesheet);
