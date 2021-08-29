c3051524f6b9cf93ba1bd771abe28be9b4269bcd
aklycbt <87492917+aklycbt@users.noreply.github.com>
2021-08-29 21:16:30 +0800

    Add files via upload

---
+++ ts3Network/server/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var port = 4200;
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var dataProcessRouter = require('./dataProcessRouter/DataProcessRouterLogin');
+
+mongoose.connect('mongodb://127.0.0.1:27017/Timesheet')
+    .then(()=>{
+        console.log("MongoDB <Timesheet> connected successfully");
+    })
+    .catch(err=>{
+        console.log('Problem connecting MongoDB <Timesheet>', err.stack);
+        process.exit(1);
+    })
+
+app.use(express.static('public'))
+app.use(cors());
+//app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.use('/', dataProcessRouter);
+
+app.listen(port, function(){
+    console.log('Listening to server port 4200 ...');
+})
